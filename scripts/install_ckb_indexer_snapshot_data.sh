e39f5120d6154f4153eada34a58e40389d3043ca
Daniel Kmak <neverkuzi@gmail.com>
2021-10-25 17:48:58 +0200

    Bump local node instructions to CKB 2021

--- scripts/install_ckb_indexer_snapshot_data.sh
+++ scripts/install_ckb_indexer_snapshot_data.sh
@@ -2,7 +2,7 @@
 
 ckbIndexerBinary="./ckb-indexer"
 indexerDataPath="./indexer-data"
-snapshot="20210822-nervos-ckb-indexer-0.2.1-testnet-snapshot.7z"
+snapshot="20211024-nervos-ckb-indexer-0.3.0-testnet-snapshot.7z"
 
 if [ ! -f ./$ckbIndexerBinary ]; then
 	echo "The ckb-indexer binary was not found in the current directory. This script should only be executed from the ckb-indexer directory."
--- scripts/install_ckb_node_snapshot_data.sh
+++ scripts/install_ckb_node_snapshot_data.sh
@@ -2,7 +2,7 @@
 
 ckbBinary="./ckb"
 indexerDataPath="./data/db"
-snapshot="20210822-nervos-ckb-node-0.43.1-testnet-snapshot.7z"
+snapshot="20211024-nervos-ckb-node-0.101.0-testnet-snapshot.7z"
 
 if [ ! -f ./$ckbBinary ]; then
 	echo "The ckb binary was not found in the current directory. This script should only be executed from the ckb directory."
