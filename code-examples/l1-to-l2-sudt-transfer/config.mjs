f5aa3cb5b575336398b888f341f9693397fd998a
Daniel Kmak <neverkuzi@gmail.com>
2021-12-10 17:32:42 +0100

    Add missing config.

--- code-examples/l1-to-l2-sudt-transfer/config.mjs
+++ code-examples/l1-to-l2-sudt-transfer/config.mjs
@@ -3,5 +3,7 @@ import { SUDT_ERC20_PROXY_ABI } from "./contracts/SudtERC20Proxy-abi.mjs";
 export const GODWOKEN_RPC_URL = 'https://godwoken-testnet-web3-rpc.ckbapp.dev';
 export const POLYJUICE_CONFIG = {
     web3Url: GODWOKEN_RPC_URL,
-    abiItems: SUDT_ERC20_PROXY_ABI
+    abiItems: SUDT_ERC20_PROXY_ABI,
+    rollupTypeHash: '0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a',
+    ethAccountLockCodeHash: '0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22',
 };
