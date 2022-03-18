d632326120e15eb3db27e34c83285bdc7221b4aa
Daniel Kmak <neverkuzi@gmail.com>
2022-03-18 14:14:30 +0100

    add check to code example check-sudt-balance

--- code-examples/5-erc20-proxy/check-sudt-balance.js
+++ code-examples/5-erc20-proxy/check-sudt-balance.js
@@ -8,6 +8,10 @@ const SUDT_PROXY_CONTRACT_ADDRESS = '<YOUR_SUDT_PROXY_CONTRACT_ADDRESS>';
 
     console.log(`Checking SUDT balance using proxy contract with address: ${SUDT_PROXY_CONTRACT_ADDRESS}...`);
 
+    if (ETHEREUM_ADDRESS === '<YOUR_ETHEREUM_ADDRESS>') {
+        throw new Error('You need to fill <YOUR_ETHEREUM_ADDRESS> before starting the program.');
+    }
+
     const provider = new ethers.providers.JsonRpcProvider('https://godwoken-testnet-web3-v1-rpc.ckbapp.dev');
     const contract = (await ethers.getContractFactory('ERC20')).attach(SUDT_PROXY_CONTRACT_ADDRESS).connect(provider);
 
