7d12d335f650d192c8ad18b766cc949e5dc03892
Daniel Kmak <neverkuzi@gmail.com>
2022-03-30 12:54:43 +0100

    fix: fix l1-to-l2 dCKB example

--- code-examples/l1-to-l2-sudt-transfer/index.mjs
+++ code-examples/l1-to-l2-sudt-transfer/index.mjs
@@ -10,6 +10,9 @@ import {
   IndexerCollector,
   RawProvider,
   SUDT,
+  Platform,
+  NervosAddressVersion,
+  LockType
 } from "@lay2/pw-core";
 import { RPC } from "ckb-js-toolkit";
 import fs from 'fs/promises';
@@ -18,13 +21,15 @@ import { SUDT_ERC20_PROXY_ABI } from "./contracts/SudtERC20Proxy-abi.mjs";
 import { GODWOKEN_RPC_URL } from "./config.mjs";
 import { getSudtIdFromTypeArgs } from "./utils.mjs";
 
+/**
+ * This script transfers SUDT from L1 account secured by PortalWallet to Godwoken (L2).
+ */
+
 const PWCore = PWCoreDefault.default;
 
 const ETHEREUM_PRIVATE_KEY =
   "0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5";
 const ETHEREUM_ADDRESS = "0xD173313A51f8fc37BcF67569b463abd89d81844f";
-const CKB_PRIVATE_KEY =
-  "0x79682c20bbcaf7fcf18eb0c69b133c872227ceb88971090e7f2242c80cd54d18";
 const SUDT_ISSUER_LOCK_HASH =
   "0xc43009f083e70ae3fee342d59b8df9eec24d669c1c3a3151706d305f5362c37e"; // aka Type Script arguments. You should find it in Layer 1 explorer SUDT Cell info.
 const SUDT_AMOUNT_TO_SEND = new Amount("1", AmountUnit.ckb); // 1 SUDT
@@ -32,10 +37,11 @@ const SUDT_NAME = "dCKB";
 const SUDT_SYMBOL = "dCKB";
 const SUDT_TOTAL_SUPPLY = "9999999999999999999";
 const SUDT_DECIMALS = 8;
+const EXISTING_SUDT_PROXY_ADDRESS = '0x884541623C1B26A926a5320615F117113765fF81'; // OPTIONAL, CAN SKIP DEPLOYMENT
 
 const _config = {
-  INDEXER_URL: "http://3.235.223.161:18116",
-  CKB_URL: "http://3.235.223.161:18114",
+  INDEXER_URL: "https://testnet.ckb.dev/indexer",
+  CKB_URL: "https://testnet.ckb.dev/rpc",
 };
 
 const web3 = new Web3(GODWOKEN_RPC_URL);
@@ -66,10 +72,13 @@ async function waitUntilCommitted(txHash, rpc, timeout = 18) {
  */
 (async () => {
   const rpc = new RPC(_config.CKB_URL);
-  const provider = new RawProvider(CKB_PRIVATE_KEY);
+  const provider = new RawProvider(ETHEREUM_PRIVATE_KEY, Platform.eth);
   await provider.init();
+  const collector = new IndexerCollector(_config.INDEXER_URL);
+  const pwCore = await new PWCore(_config.CKB_URL).init(provider, collector);
+  
   const ckbAddress = provider.address;
-  console.log(`Transferring from CKB address: ${ckbAddress.addressString}`);
+  console.log(`Transferring from CKB address: ${ckbAddress.toCKBAddress(NervosAddressVersion.ckb2021, LockType.pw)}`);
 
   const addressTranslator = new AddressTranslator();
   await addressTranslator.init('testnet');
@@ -82,15 +91,16 @@ async function waitUntilCommitted(txHash, rpc, timeout = 18) {
     `Deposit to Layer 2 address on Layer 1: \n${layer2depositAddress}`
   );
 
-  const collector = new IndexerCollector(_config.INDEXER_URL);
-  const pwCore = await new PWCore(_config.CKB_URL).init(provider, collector);
-
   const sudt = new SUDT(SUDT_ISSUER_LOCK_HASH);
-  const sudtBalance = await collector.getSUDTBalance(sudt, ckbAddress);
+  const sudtBalance = await collector.getSUDTBalance(sudt, new Address(ckbAddress.toCKBAddress(NervosAddressVersion.pre2021, LockType.pw), AddressType.ckb));
   const options = {
-    witnessArgs: Builder.WITNESS_ARGS.RawSecp256k1,
+    witnessArgs: Builder.WITNESS_ARGS.Secp256k1Pw,
     autoCalculateCapacity: true,
-    minimumOutputCellCapacity: new Amount('400', AmountUnit.ckb)
+    minimumOutputCellCapacity: new Amount('400', AmountUnit.ckb),
+    defaultCollectorOptions: {
+      lockType: LockType.pw
+    },
+    changeCellLockType: LockType.pw
   };
 
   console.log(`SUDT balance: ${sudtBalance}`);
@@ -112,7 +122,7 @@ async function waitUntilCommitted(txHash, rpc, timeout = 18) {
     sudt,
     new Address(layer2depositAddress, AddressType.ckb),
     SUDT_AMOUNT_TO_SEND,
-    true,
+    false,
     null,
     options
   );
@@ -141,33 +151,38 @@ async function waitUntilCommitted(txHash, rpc, timeout = 18) {
     return;
   }
 
-  console.log(`Deploying contract...`);
+  let contract;
+  if (EXISTING_SUDT_PROXY_ADDRESS) {
+    contract = new web3.eth.Contract(SUDT_ERC20_PROXY_ABI, EXISTING_SUDT_PROXY_ADDRESS);
+  } else {
+    console.log(`Deploying contract...`);
 
-  const deployTx = new web3.eth.Contract(SUDT_ERC20_PROXY_ABI)
-    .deploy({
-      data: SudtProxyBytecode,
-      arguments: [
-        SUDT_NAME,
-        SUDT_SYMBOL,
-        SUDT_TOTAL_SUPPLY,
-        sudtId,
-        SUDT_DECIMALS,
-      ],
-    })
-    .send({
-      from: account.address,
-      gas: 6000000
-    });
+    const deployTx = new web3.eth.Contract(SUDT_ERC20_PROXY_ABI)
+      .deploy({
+        data: SudtProxyBytecode,
+        arguments: [
+          SUDT_NAME,
+          SUDT_SYMBOL,
+          SUDT_TOTAL_SUPPLY,
+          sudtId,
+          SUDT_DECIMALS,
+        ],
+      })
+      .send({
+        from: account.address,
+        gas: 6000000
+      });
 
-  deployTx.on("transactionHash", (hash) =>
-    console.log(`Layer 2 SUDT-ERC20 Proxy deploy transaction hash: ${hash}`)
-  );
+    deployTx.on("transactionHash", (hash) =>
+      console.log(`Layer 2 SUDT-ERC20 Proxy deploy transaction hash: ${hash}`)
+    );
 
-  const contract = await deployTx;
+    contract = await deployTx;
 
-  console.log(
-    `Deployed SUDT-ERC20 Proxy contract address: ${contract.options.address}`
-  );
+    console.log(
+      `Deployed SUDT-ERC20 Proxy contract address: ${contract.options.address}`
+    );
+  }
 
   console.log(`Getting balance on Layer 2...`);
 
